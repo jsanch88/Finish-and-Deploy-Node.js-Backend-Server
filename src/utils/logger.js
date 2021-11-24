1beb0d4418db8ab61eb18262c8242e02dfabf9b4
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-23 23:15:18 -0600

    Final Node.js-Backend-Server

---
+++ src/utils/logger.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import morgan from 'morgan'
+import consola from 'consola'
+
+const middleware = morgan('dev')
+const log = consola
+export default { middleware, log }
