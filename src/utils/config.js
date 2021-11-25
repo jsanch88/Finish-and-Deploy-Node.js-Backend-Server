39ee3f74eada8c4020715783faa4ed0be4fb5dbb
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-24 22:58:50 -0600

    Auth completed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import dotenv from 'dotenv'
 import joi from 'joi'
 
@@ -9,13 +8,12 @@ dotenv.config()
 const envSchema = joi
   .object()
   .keys({
-    NODE_ENV: joi
-      .string()
-      .valid('development', 'production')
-      .default('development'),
+    NODE_ENV: joi.string().valid('development', 'production').required(),
     PORT: joi.number().positive().required(),
     ORIGIN: joi.string().uri().required(),
     DATABASE_URL: joi.string().uri().required(),
+    API_PASS: joi.string().required(),
+    API_USER: joi.string().required(),
   })
   .unknown()
 
@@ -23,8 +21,14 @@ const { value: env, error } = envSchema
   .prefs({ errors: { label: 'key' } })
   .validate(process.env)
 
-  if (error) {
-    logger.log.error(new Error(`Config validation error: ${error.message}`))
-  }
+if (error) {
+  logger.log.error(new Error(`Config validation error: ${error.message}`))
+}
 
-export default { nodeEnv: env.NODE_ENV, port: env.PORT }
+export default {
+  nodeEnv: env.NODE_ENV,
+  port: env.PORT,
+  origin: env.ORIGIN,
+  apiUser: env.API_USER,
+  apiPass: env.API_PASS,
+}
